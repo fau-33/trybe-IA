c6aa14b1bb3ed2b39cf2e3fc6695a4c856df25c6
Flávio Félix <fau_felix30@hotmail.com>
2024-12-23 15:42:25 -0300

    Acrescentando no chatbot arquivos funcionalidades

--- chat/chat.py
+++ chat/chat.py
@@ -13,9 +13,20 @@ initial_prompt = "Você é um consultor de desenvolvimento de projetos."
 chat.send_message(initial_prompt)
 
 def gradio_wrapper(message, _history):
-    response = chat.send_message(message)
+    text = message["text"]
+    ulploaded_files = []
+    
+    for files_info in message["files"]:
+        file_path = files_info["path"]
+        ulploaded_file_info = genai.upload_file(file_path)
+        ulploaded_files.append(ulploaded_file_info)
+    
+    prompt = [message["text"]]
+    prompt.extend(ulploaded_files)  
+        
+    response = chat.send_message(prompt)
     return response.text
 
 
-chatInterface = gr.ChatInterface(gradio_wrapper)
+chatInterface = gr.ChatInterface(fn=gradio_wrapper, multimodal=True)
 chatInterface.launch()
